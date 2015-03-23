7af2bbf44fb1e1cac284ee1fee95d47e5ab0c113
Tania <tania@efaber.net>
2015-03-23 16:35:17 +0100

    Remove environment attribute references in spree_trackers

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -4,8 +4,8 @@ Spree::PaymentMethod.class_eval do
 
   def self.available(display_on = 'both', store = nil)
     result = all.select do |p|
+      # (p.environment == Rails.env || p.environment.blank?) &&
       p.active &&
-        (p.environment == Rails.env || p.environment.blank?) &&
         (store.nil? || store.payment_methods.empty? || store.payment_methods.include?(p)) &&
         (p.display_on == display_on.to_s || p.display_on.blank?)
     end
--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -2,6 +2,7 @@ Spree::Tracker.class_eval do
   belongs_to :store
 
   def self.current(domain)
-    Spree::Tracker.where(:active => true, :environment => Rails.env).joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
+    # , :environment => Rails.env
+    Spree::Tracker.where(:active => true).joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
   end
 end
