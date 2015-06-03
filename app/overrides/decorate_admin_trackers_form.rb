e18f7f20fcef0c03eab54c0a72c09c080bed4b8e
Josua Schmid <josua.schmid@renuo.ch>
2015-06-03 15:31:23 +0200

    patch store tracker form in a better way

--- app/overrides/decorate_admin_trackers_form.rb
+++ app/overrides/decorate_admin_trackers_form.rb
@@ -1,6 +1,5 @@
 Deface::Override.new(
   :virtual_path => "spree/admin/trackers/_form",
   :name => "multi_domain_additional_tracker_fields",
-  :replace => "[data-hook='additional_tracker_fields']",
-  :partial => "spree/admin/trackers/store",
-  :disabled => false)
+  :insert_bottom => "[data-hook='additional_tracker_fields']",
+  :partial => "spree/admin/trackers/store")
